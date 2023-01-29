d60cd6d00449d6075df4a532b4b7c9b8e6ba8438
BuildTools <unconfigured@null.spigotmc.org>
2023-01-28 22:15:10 -0800

    be nice 2 me

--- src/subsytems/flywheel.cpp
+++ src/subsytems/flywheel.cpp
@@ -17,6 +17,7 @@ double tbh=0;
 double prev_error=0;
 bool initiaized=false;
 double k=0;
+double v=0;
 
 int Time = pros::millis();
 Motor flywheel(10,true,AbstractMotor::gearset::blue,AbstractMotor::encoderUnits::degrees);
@@ -129,10 +130,11 @@ void TBH(void* ){
     //flexwheel mass is 117.934grams 
 
 while(true){
+    v=targetTBH*19+422;
    pros::delay(10);
    double k=1.2/(2*targetTBH);
    error=targetTBH-flywheel.getActualVelocity();
-   output=(((targetTBH/1+pow((2.718),(-k*error)))*20)+(targetTBH*6));
+   output=(((targetTBH/1+pow((2.718),(-k*error)))*20)+(v));
 }
 }
 
