085376294e4a05a6d4b478a6c1eed042bf809873
Archer2305 <73808514+Archer2305@users.noreply.github.com>
2023-02-06 13:18:30 -0800

    Commented DriveToPoint

--- include/subsytems/autonFunctions.hpp
+++ include/subsytems/autonFunctions.hpp
@@ -10,7 +10,7 @@ void driveBlorward(double distance, double scalar);
 void bangBang();
 extern double targetF;
 void driveBackward(double distance, double scalar);    
-void driveToPoint(double posX, double posY,bool backward,double speed);
+// void driveToPoint(double posX, double posY,bool backward,double speed);
 extern double ogXPos;
 extern double ogYPost;
 extern double distance;
